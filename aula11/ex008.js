fc80934e946640609e9458001398332d773f86d5
Dayvid Ornelas <dayvidcristhian19@gmail.com>
2024-01-05 10:35:33 -0300

    Aulas do curso de Java Script básico, Curso em Vídeo

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 89
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { ///condição simpmles
+    console.log(`Você utrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
